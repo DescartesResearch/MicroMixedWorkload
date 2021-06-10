72ab84a1a670cdd2fbbe1158e72d63e6b7c01b3a
Norbert Schmitt <norbert.schmitt@uni-wuerzburg.de>
2021-06-10 16:13:26 +0200

    Added a convenience function if the PID file uses the standard directory and filename. Also added a missing comment.

--- src/main/java/microbenchmark/PID.java
+++ src/main/java/microbenchmark/PID.java
@@ -5,6 +5,7 @@ import com.sun.jna.platform.win32.Kernel32;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.FileSystems;
 import java.nio.file.Path;
 
 public class PID {
@@ -13,6 +14,11 @@ public class PID {
 	 * The Java system property string to retrieve the operating system Java is running on.
 	 */
 	public static final String OS_NAME_PROPERTY = "os.name";
+
+	/**
+	 * Standard filepath for the PID file.
+	 */
+	public static final Path STD_PID_FILEPATH = FileSystems.getDefault().getPath("pid.txt");
 
 	/**
 	 * Returns the PID of this process. Returns -1 if the OS is unknown.
@@ -29,6 +35,11 @@ public class PID {
 		return pid;
 	}
 
+	/**
+	 * Function to write PID to a textfile at the given filename and directory.
+	 * @param filePath Path to the file containing the PID number.
+	 * @param pid PID as integer that will be written to the textfile.
+	 */
 	public static void writePID(Path filePath, int pid) {
 		if (filePath == null) {
 			throw new IllegalArgumentException("Path to PID file is null.");
@@ -41,5 +52,11 @@ public class PID {
 		}
 	}
 
-
+	/**
+	 * Convenience function if we want to write to the standard directory and the standard filename.
+	 * @param pid PID as integer that will be written to the textfile.
+	 */
+	public static void writePID(int pid) {
+		writePID(STD_PID_FILEPATH, pid);
+	}
 }
